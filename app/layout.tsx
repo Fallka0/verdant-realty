a3bf1ce142f5c314e24698d0dba84d86a55a4e19
Mykyta Pantelei <69463681+Fallka0@users.noreply.github.com>
2026-04-20 08:59:13 +0200

    feat: add Lato as secondary font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,9 @@
 import type { Metadata } from "next";
 
+import "@fontsource/lato/300.css";
+import "@fontsource/lato/400.css";
+import "@fontsource/lato/700.css";
+import "@fontsource/lato/900.css";
 import "./globals.css";
 
 export const metadata: Metadata = {
